066c8af3a4f08b77ba9e715306aaf89fc9141676
mackilroy007 <mackilroy007@gmail.com>
2020-10-13 15:37:30 +0200

    not able to display values on the for in loop. All the others are working

---
+++ basic3/typescript.js
@@ -0,0 +1,13 @@
+//array with names
+var ArrayNumbers = ["Steve", "Alfred", "James", "Zuse", "Hermes", "Platos", "Madusa", "Nike", "Athena", "John"];
+for (var obj in ArrayNumbers) {
+    //print out the index
+    // console.log(index);
+    //print out values
+    console.log(obj[0]);
+} //adfadsf
+//print out the value
+for (var _i = 0, ArrayNumbers_1 = ArrayNumbers; _i < ArrayNumbers_1.length; _i++) {
+    var i = ArrayNumbers_1[_i];
+    // console.log(i);
+}
---
+++ basic3/typescript.ts
@@ -0,0 +1,16 @@
+//array with names
+var ArrayNumbers : Array<string>=["Steve","Alfred","James","Zuse","Hermes","Platos","Madusa","Nike","Athena","John"];
+
+
+for (let obj in ArrayNumbers){
+    //print out the index
+    // console.log(index);
+    //print out values
+    console.log(obj[0]);
+}//adfadsf
+
+//print out the value
+for (let i of ArrayNumbers){
+    // console.log(i);
+}
+
